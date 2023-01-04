dfa3f052b5a9b5f7e9136ae0c8b0f4acc8b02174
almaz <almazbekovaskar30@gmail.com>
2023-01-04 20:13:36 +0100

    Implemented interface ClientService to class ClientServiceImpl

--- src/service/impl/ClientServiceImpl.java
+++ src/service/impl/ClientServiceImpl.java
@@ -1,5 +1,45 @@
 package service.impl;
 
-public class ClientServiceImpl {
+import service.ClientService;
 
+public class ClientServiceImpl implements ClientService {
+
+    public ClientServiceImpl() {
+        super();
+    }
+
+    @Override
+    public String addClient(Client client) {
+        return null;
+    }
+
+    @Override
+    public String addClient(List<Client> clients) {
+        return null;
+    }
+
+    @Override
+    public List<Client> getClientByName(String name) {
+        return null;
+    }
+
+    @Override
+    public Client removeClientById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Taxi orderTaxi(Long clientId, String taxiType) {
+        return null;
+    }
+
+    @Override
+    public Map<Integer, Client> getClientAge() {
+        return null;
+    }
+
+    @Override
+    public void universalSorting(String word) {
+
+    }
 }
