8ef7098003dff0a72ccab137a0d80fcc702b3254
ShabdanovIlim <sabdanovilim@gmail.com>
2023-01-05 19:40:31 +0600

    driver method realization

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,155 @@
+import classes.Client;
+import classes.Driver;
+import classes.License;
+import classes.Taxi;
+import enums.Gender;
+import enums.TaxiType;
+import service.impl.ClientServiceImpl;
+import service.impl.DriverServiceImpl;
+import service.impl.TaxiServiceImpl;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+    public static ClientServiceImpl clientService = new ClientServiceImpl();
+    public static DriverServiceImpl driverService = new DriverServiceImpl();
+    public static TaxiServiceImpl taxiService = new TaxiServiceImpl();
+
     public static void main(String[] args) {
 
 
+        License license = new License(3L, LocalDate.of(2018, 2, 4), LocalDate.of(2028, 3, 5));
+        License license2 = new License(88L, LocalDate.of(2019, 2, 4), LocalDate.of(2038, 3, 5));
+        Taxi taxi = new Taxi(4L, "BMW", "07kg 156 aac", "red", LocalDate.of(2013, 5, 7), TaxiType.BUSINESS);
+        Taxi taxi2 = new Taxi(21L, "FIT", "07kg 234 aac", "black", LocalDate.of(2019, 5, 7), TaxiType.COMFORT);
+        Taxi taxi3 = new Taxi(14L, "MERS", "07kg 334 aac", "blue", LocalDate.of(2016, 5, 7), TaxiType.STANDART);
+        List<Taxi> taxis = new ArrayList<>(List.of(taxi2, taxi3));
+
+        Client client = new Client(1L, "ShabdanovIlim", LocalDate.of(2003, 10, 3), "+996123456", BigDecimal.valueOf(1000));
+        Client client2 = new Client(7L, "AltynbekovAlibek", LocalDate.of(2002, 10, 3), "+99622323456", BigDecimal.valueOf(2000));
+        List<Client> clients = new ArrayList<>(List.of(client));
+
+        Driver driver = new Driver(2L, "Nurik", "Djoldoshov", Gender.MALE, "+9962344332", license, BigDecimal.valueOf(5000));
+        Driver driver2 = new Driver(22L, "Dastan", "Akbaraliev", Gender.MALE, "+9968944332", license2, BigDecimal.valueOf(1500));
+        List<Driver> drivers = new ArrayList<>(List.of(driver2));
+
+        while (true) {
+            System.out.println("""
+                    =====CLIENT COMMANDS=====
+                    1.ADD CLIENT MAIN 
+                    2.ADD CLIENT LIST
+                    3.GET CLIENT BY NAME :
+                    4.REMOVE CLIENT BY ID 
+                    5.ORDER TAXI
+                    6.GET CLIENT AGE
+                    7.UNIVERSAL SORTING :
+                    =====DRIVER COMMANDS=====
+                    8.ADD DRIVER MAIN
+                    9.ADD DRIVER LIST
+                    10.FIND BY ID
+                    11.FIND BY NAME
+                    12.ASSIGN TAXI TO DRIVER
+                    13.CHANGE TAXI OR DRIVER
+                    14.GET DRIVER BY TAXI MODEL
+                    15.UPDATE DRIVER :
+                    =====TAXI COMMANDS=====
+                    16.ADD TAXI MAIN
+                    17.ADD TAXI LIST
+                    18.FIND BY INITIAL LETTER 
+                    19.TAXI GROUPING
+                    20.FILTER BY TAXI TYPE
+                    21.UPDATE TAXI ID 
+                                    
+                    """);
+            System.out.println("Enter by command :");
+            int a = new Scanner(System.in).nextInt();
+            switch (a) {
+                case 1 -> System.out.println(clientService.addClient(client2));
+                case 2 -> System.out.println(clientService.addClient(clients));
+                case 3 -> {
+                    System.out.println("Enter by name :");
+                    String name = new Scanner(System.in).nextLine();
+                    System.out.println(clientService.getClientByName(name));
+                }
+                case 4 -> {
+                    System.out.println("Enter by id :");
+                    Long id = new Scanner(System.in).nextLong();
+                    System.out.println(clientService.removeClientById(id));
+                }
+                case 5 -> {
+                    System.out.println("Enter by client id :");
+                    Long id = new Scanner(System.in).nextLong();
+                    System.out.println("Enter by taxi type :");
+                    String type = new Scanner(System.in).nextLine();
+                    System.out.println(clientService.orderTaxi(id, type));
+                }
+                case 6 -> System.out.println(clientService.getClientAge());
+                case 7 -> {
+                    System.out.println("Enter by command : (id / fullName / dateOfBirth / phoneNumber / money)");
+                    System.out.println("Enter by command :");
+                    String word = new Scanner(System.in).nextLine();
+                    clientService.universalSorting(word);
+                }
+                case 8 -> System.out.println(driverService.add(driver));
+                case 9 -> System.out.println(driverService.add(drivers));
+                case 10 -> {
+                    System.out.println("Enter by id :");
+                    Long id = new Scanner(System.in).nextLong();
+                    System.out.println(driverService.findById(id));
+                }
+                case 11 -> {
+                    System.out.println("Enter by name :");
+                    String name = new Scanner(System.in).nextLine();
+                    System.out.println(driverService.findByName(name));
+                }
+                case 12 -> {
+                    System.out.println("Enter by taxi name :");
+                    String taxiname = new Scanner(System.in).nextLine();
+                    System.out.println("Enter by long :");
+                    Long id = new Scanner(System.in).nextLong();
+                    System.out.println(driverService.assignTaxiToDriver(taxiname, id));
+                }
+                case 13 -> {
+                    System.out.println("Enter by driver id : ");
+                    Long driverid = new Scanner(System.in).nextLong();
+                    System.out.println("Enter by taxi id :");
+                    Long taxiid = new Scanner(System.in).nextLong();
+                    System.out.println(driverService.changeTaxiOrDriver(driverid, taxiid));
+                }
+                case 14 -> {
+                    System.out.println("Enter by model :");
+                    String model = new Scanner(System.in).nextLine();
+                    System.out.println(driverService.getDriverByTaxiModel(model));
+                }
+                case 15 -> {
+                    System.out.println("Enter by driver name :");
+                    String driverName = new Scanner(System.in).nextLine();
+                    driverService.update(driverName);
+                }
+                case 16 -> System.out.println(taxiService.add(taxi));
+                case 17 -> System.out.println(taxiService.add(taxis));
+                case 18 -> {
+                    System.out.println("Enter by initial letter model :");
+                    String l = new Scanner(System.in).nextLine();
+                    System.out.println(taxiService.findByInitialLetter(l));
+                }
+                case 19-> System.out.println(taxiService.grouping());
+                case 20->{
+                    System.out.println("Select taxi type (standart / comfort / business)");
+                    String com = new Scanner(System.in).nextLine();
+                    System.out.println(taxiService.filterByTaxiType(com));
+                }
+                case 21->{
+                    System.out.println("Enter by id :");
+                    Long id = new Scanner(System.in).nextLong();
+                    taxiService.update(id);
+                }
+                default -> System.out.println("No such command !");
+            }
+        }
     }
 }
--- src/service/impl/ClientServiceImpl.java
+++ src/service/impl/ClientServiceImpl.java
@@ -1,48 +1,142 @@
 package service.impl;
 
 import classes.Client;
+import classes.Driver;
 import classes.Taxi;
 import dao.Database;
 import service.ClientService;
 
-import java.util.List;
-import java.util.Map;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.*;
 
 public class ClientServiceImpl implements ClientService {
     Database database = new Database();
 
     @Override
     public String addClient(Client client) {
-        return null;
+        try{
+            for (Client databaseClient : database.getClients()) {
+                if(client.getId().equals(databaseClient.getId()))
+                    throw new Exception("Duplicate Client ID. Try another ID.");
+            }
+            database.getClients().add(client);
+            return "Client added successfully!";
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return "Something went wrong. (addClient Method)";
     }
+
 
     @Override
     public String addClient(List<Client> clients) {
-        return null;
+        database.getClients().addAll(clients);
+        return "Successfully Added ! ";
     }
 
     @Override
     public List<Client> getClientByName(String name) {
-        return null;
+        List<Client> list = database.getClients().stream().filter(x -> x.getFullName().contains(name)).toList();
+        return list;
     }
 
     @Override
     public Client removeClientById(Long id) {
-        return null;
+        Client list = database.getClients().stream().filter(x -> x.getId().equals(id)).findAny().get();
+        database.getClients().remove(list);
+        return list;
     }
 
     @Override
     public Taxi orderTaxi(Long clientId, String taxiType) {
-        return null;
+        Taxi taxi = new Taxi();
+        Driver driver = new Driver();
+        int sum = 0;
+        try {
+            for (Client client : database.getClients()) {
+                if (client.getId().equals(clientId)) {
+                    throw new Exception();
+                } else {
+                    System.out.println("Enter by command : (standart / comfort / business)");
+                    if (taxiType.equals("business")) {
+                        System.out.println(" Enter by km !");
+                        int km = new Scanner(System.in).nextInt();
+                        int k = taxi.getTaxiType().getPricePerKm().intValue();
+                        sum = k * km;
+                        int mon = client.getMoney().intValue();
+                        client.setMoney(BigDecimal.valueOf(mon-sum));
+
+                    }
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Wrong input ! ");
+                    }
+        return taxi;
     }
 
     @Override
     public Map<Integer, Client> getClientAge() {
-        return null;
+        Map<Integer, Client> map = new LinkedHashMap<>();
+        for (Client client : database.getClients()) {
+            int period = Period.between(client.getDateOfBirth(), LocalDate.now()).getYears();
+            map.put(period, client);
+        }
+        return map;
     }
 
     @Override
     public void universalSorting(String word) {
+        switch (word) {
+            case "id" -> {
+                System.out.println("Enter by command : (ascending / descending)");
+                String w = new Scanner(System.in).nextLine();
+                if (w.equals("ascending")) {
+                    database.getClients().stream().sorted(Comparator.comparing(Client::getId)).forEach(System.out::println);
+                } else if (w.equals("descending")) {
+                    database.getClients().stream().sorted(Comparator.comparing(Client::getId).reversed()).forEach(System.out::println);
+                }
+            }
+            case "fullName" -> {
+                System.out.println("Enter by command : (ascending / descending)");
+                String w = new Scanner(System.in).nextLine();
+                if (w.equals("ascending")) {
+                    database.getClients().stream().sorted(Comparator.comparing(Client::getFullName)).forEach(System.out::println);
+                } else if (w.equals("descending")) {
+                    database.getClients().stream().sorted(Comparator.comparing(Client::getFullName).reversed()).forEach(System.out::println);
+                }
+            }
+            case "dateOfBirth" -> {
+                System.out.println("Enter by command : (ascending / descending)");
+                String w = new Scanner(System.in).nextLine();
+                if (w.equals("ascending")) {
+                    database.getClients().stream().sorted(Comparator.comparing(Client::getDateOfBirth)).forEach(System.out::println);
+                } else if (w.equals("descending")) {
+                    database.getClients().stream().sorted(Comparator.comparing(Client::getDateOfBirth).reversed()).forEach(System.out::println);
+                }
+            }
+            case "phoneNumber" -> {
+                System.out.println("Enter by command : (ascending / descending)");
+                String w = new Scanner(System.in).nextLine();
+                if (w.equals("ascending")) {
+                    database.getClients().stream().sorted(Comparator.comparing(Client::getPhoneNumber)).forEach(System.out::println);
+                } else if (w.equals("descending")) {
+                    database.getClients().stream().sorted(Comparator.comparing(Client::getPhoneNumber).reversed()).forEach(System.out::println);
+                }
+            }
+            case "money" -> {
+                System.out.println("Enter by command : (ascending / descending)");
+                String w = new Scanner(System.in).nextLine();
+                if (w.equals("ascending")) {
+                    database.getClients().stream().sorted(Comparator.comparing(Client::getMoney)).forEach(System.out::println);
+                } else if (w.equals("descending")) {
+                    database.getClients().stream().sorted(Comparator.comparing(Client::getMoney).reversed()).forEach(System.out::println);
+                }
+            }
+            default -> System.out.println("NO SUCH COMMAND !");
+        }
 
     }
 }
