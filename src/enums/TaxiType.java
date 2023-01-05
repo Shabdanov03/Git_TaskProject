bfe7cf2cad09f897545415a54611a4da42c46638
User <sabdanovilim@gmail.com>
2023-01-05 11:09:47 +0600

    added pole for enums

--- src/enums/TaxiType.java
+++ src/enums/TaxiType.java
@@ -1,4 +1,32 @@
 package enums;
 
+import java.math.BigDecimal;
+
 public enum TaxiType {
+    STANDART(BigDecimal.valueOf(15),BigDecimal.valueOf(70)),
+    COMFORT(BigDecimal.valueOf(30),BigDecimal.valueOf(130)),
+    BUSINESS(BigDecimal.valueOf(40),BigDecimal.valueOf(150));
+    BigDecimal pricePerKm;
+    BigDecimal priceForLanding;
+
+    TaxiType(BigDecimal pricePerKm, BigDecimal priceForLanding) {
+        this.pricePerKm = pricePerKm;
+        this.priceForLanding = priceForLanding;
+    }
+
+    public BigDecimal getPricePerKm() {
+        return pricePerKm;
+    }
+
+    public void setPricePerKm(BigDecimal pricePerKm) {
+        this.pricePerKm = pricePerKm;
+    }
+
+    public BigDecimal getPriceForLanding() {
+        return priceForLanding;
+    }
+
+    public void setPriceForLanding(BigDecimal priceForLanding) {
+        this.priceForLanding = priceForLanding;
+    }
 }
